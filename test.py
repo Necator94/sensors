8df222f357fe79be98391d88dba1a6cf0d838da7
root <root@beaglebone.localdomain>
2016-04-02 08:55:33 +0000

    polling

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
 import Adafruit_BBIO.GPIO as GPIO
+import time
  
-GPIO.setup("P8_10", GPIO.OUT)
-GPIO.output("P8_10", GPIO.HIGH)
-GPIO.cleanup()
+GPIO.setup("P8_12", GPIO.IN)     # out pin
+GPIO.setup("P8_14", GPIO.OUT)     # EN  pin 
+GPIO.setup("P8_16", GPIO.OUT)     # LED  pin 
+
+GPIO.output("P8_14", GPIO.HIGH)
+while True:
+	if GPIO.input("P8_12"):
+    		GPIO.output("P8_16", GPIO.HIGH)
+		print("HIGH")
+		time.sleep(1)
+	else:
+		GPIO.output("P8_16", GPIO.LOW)
+   		print("LOW")
