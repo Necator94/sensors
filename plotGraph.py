37369496b32a0ff2c89c6fe052b4b4f257900319
root <root@beaglebone.localdomain>
2016-04-12 16:24:55 +0000

    plotgraph

--- plotGraph.py
+++ plotGraph.py
@@ -12,24 +12,29 @@ srfDistance_ = []
 
 xBandtxt = open("xBandData.txt", "r")
 for line in xBandtxt:
-        xBandData,xBandTime = line.split()
+	xBandData,xBandTime = line.split()
 	xBandData_.append(xBandData)
 	xBandTime_.append(xBandTime)		
 xBandtxt.close()
 
 pirtxt = open("pirData.txt", "r")
 for line in pirtxt:
-        pirData,pirTime = line.split()
+    	pirData,pirTime = line.split()
 	pirData_.append(pirData)
 	pirTime_.append(pirTime)		
 pirtxt.close()
 
 srf08txt = open("srf08Data.txt", "r")
 for line in srf08txt:
-        srf08Data,srf08Time = line.split()
+	srf08Data,srf08Time = line.split()
 	srf08Data_.append(srf08Data)
-	srf08Time_.append(srf08Time)		
+	srf08Time_.append(srf08Time)
 srf08txt.close()
+
+srf08Distancetxt = open("srf08Distance.txt", "r")
+for line in srf08Distancetxt:
+	srfDistance_.append(line)		
+srf08Distancetxt.close()
 
 plt.figure(1)
 plt.subplots_adjust(hspace=.4)
@@ -37,6 +42,10 @@ plt.subplots_adjust(hspace=.4)
 #srf08
 plt.subplot(311)
 plt.plot(srf08Time_, srf08Data_, 'g')
+plt.scatter(srf08Time_, srf08Data_)
+for k in xrange(len(srfDistance_)):
+	if float(srf08Data_[k]) != 0:
+    		plt.text(srf08Time_[k],float(srf08Data_[k]) + 0.1, srfDistance_[k], rotation='vertical', fontsize=8, horizontalalignment='left',verticalalignment='bottom',)
 plt.axis([0,10,0,1.5])
 plt.ylabel('Motion status')
 plt.title('Ultrasonic SRF08 sensor')
@@ -58,5 +67,3 @@ plt.title('X-band motion sensor')
 
 plt.show()
 plt.savefig("Plot.svg")
-
-
