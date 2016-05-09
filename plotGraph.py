d47c42bfa2040e2fe13df9c4aba33564580f643c
Ivan <Necator94@gmail.com>
2016-05-09 19:11:47 +0200

    last modification

--- plotGraph.py
+++ plotGraph.py
@@ -5,9 +5,9 @@ from matplotlib.gridspec import GridSpec
 from matplotlib.gridspec import GridSpec
 matplotlib.rcParams.update({"figure.figsize": (20.0,12.0)})
 
-#sys.argv[1] - number of experiment
-#sys.argv[2] - X-band sensetivity
-#sys.argv[3] - path to saved file
+#sys.argv[1] - distance to motion
+#sys.argv[2] - comment to plot, e.g. X-band sensetivity
+#sys.argv[3] - path to save file
 
 xBandData = []
 xBandTime = []
@@ -28,10 +28,8 @@ plt.subplots_adjust(hspace = .4)
 
 tl = 0
 th = 30
-if int (sys.argv[1]) < 2 :
-	plt.suptitle("Sensors response for " + sys.argv[1] + " meter, " + sys.argv[2], fontsize = 18)
-else:
-	plt.suptitle("Sensors response for " + sys.argv[1] + " meters, " + sys.argv[2], fontsize = 18)
+
+plt.suptitle("Sensors response for " + sys.argv[1] + ", " + sys.argv[2], fontsize = 15)
 #srf08
 gs1 = GridSpec(2, 1)
 gs1.update(left=0.03, right=0.98, wspace=0)
@@ -49,8 +47,6 @@ plt.ylabel('Motion status')
 plt.xlabel('Time, s')
 plt.title('X-band motion sensor')
 
-if int (sys.argv[1]) < 10 :
-	plt.savefig(sys.argv[3] + "0" + sys.argv[1] + "_meters.png")
-else :
-	plt.savefig(sys.argv[3] + sys.argv[1] + "_meters.png")
+plt.savefig(sys.argv[3] + sys.argv[1] + ".png")
+
 plt.show()
