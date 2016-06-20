9e2f6ea8d9d89a1786d6d86fe3fd7c669b64be49
Ivan <Necator94@gmail.com>
2016-06-20 14:28:07 +0200

    working lab version

--- main_BBB.py
+++ main_BBB.py
@@ -13,6 +13,11 @@ import os
 import math
 import numpy as np
 
+
+#sys.argv[1] - time duration of the experiment
+#sys.argv[2] - frequency criteria of movment detection
+#sys.argv[3] - standard deviation criteria of movement detection
+
 # 0 - out pin     1 - LED pin
 xBandPins = {0 : 'P8_12', 1 : 'P8_11'}	
 pirPins = {0 : 'P8_14', 1 : 'P8_13' }
@@ -29,8 +34,11 @@ GPIO.setup(xBandPins[1], GPIO.OUT)
 GPIO.setup(pirPins[0], GPIO.IN)	    	
 GPIO.setup(pirPins[1], GPIO.OUT)    	
 
-def xband_pir(pin, cycles, out_raw_data, out_fr_trans_graph, name):
-	#window = int(sys.argv[2])
+def xband_pir(pin, out_raw_data, out_fr_trans_graph, out_detect_signal,  name):
+
+	duration = float(sys.argv[1])
+	fr_level = float(sys.argv[2])
+	std_level = float(sys.argv[3])
 	periods = []
 	temp = []
 	temp.append([])
@@ -44,12 +52,20 @@ def xband_pir(pin, cycles, out_raw_data, out_fr_trans_graph, name):
 	t_time = 0
 
 	slide_window = []
-	temp_slide_window = []
+	detect_signal = []
+	detect_signal.append([])
+	detect_signal.append([])
+	detect_signal.append([])
+	detect_signal.append([])
+	a = 0
+	st_dev = 0
+	mean_vol = 0
+	mean_window = []
 
 	print name, 'started'
 	
 	startTime = time.time()
-	while t_time < cycles:
+	while t_time < duration:
 		check = GPIO.input(pin[0])
 		t_time = time.time() - startTime
 		raw_data[0].append(check)	
@@ -62,38 +78,52 @@ def xband_pir(pin, cycles, out_raw_data, out_fr_trans_graph, name):
 			periods.append(temp[1][-2]) 
 			if len(periods) > 1:
 				freq = 1/(periods[-1] - periods[-2])
+#___________________________________________________
 				fr_trans_graph[0].append(temp[1][-2])
 				fr_trans_graph[1].append(freq)
 				slide_window.append(freq)
-				if len(slide_window) > 10:
-					del slide_window[0]
+				mean_window.append(freq)
+				
+				if len(slide_window) > 3:
+					#del slide_window[0]
+					slide_window = [] 
+					mean_window = []
 				# standart deviation
-				if len(slide_window) == 10: 
-					#print np.std(slide_window), freq
-					a = reject_outliers(slide_window)
-					print len(a)
-				if freq > 30:
+				if len(slide_window) == 3: 
+					st_dev = np.std(slide_window)
+					mean_vol = sum(mean_window)/len(mean_window)
+				if mean_vol > fr_level and st_dev < std_level:
 					GPIO.output(pin[1], GPIO.HIGH)
+					detect_signal[0].append(t_time)
+					detect_signal[1].append(10)
+					detect_signal[2].append(mean_vol)
+					detect_signal[3].append(st_dev)
 				else:
 					GPIO.output(pin[1], GPIO.LOW)
+					detect_signal[0].append(t_time)
+					detect_signal[1].append(0)
+					detect_signal[2].append(mean_vol)
+					detect_signal[3].append(st_dev)
 				#print freq, periods[-1], len(periods)
 				del periods[0]
 			del temp[0][:-1]
 			del temp[1][:-1]
+			#st_dev = 0
 
 		
-
 
 	print name, 'finished'
 	out_raw_data.put(raw_data)
 	out_fr_trans_graph.put(fr_trans_graph)
+	out_detect_signal.put(detect_signal)
 
 
 xBand_raw_data_queue = Queue.Queue()
 xBand_fr_transform_queue = Queue.Queue()
+xBand_detect_signal_queue = Queue.Queue()
 #pirData_queue = Queue.Queue()
 
-xBandThread = threading.Thread(target = xband_pir, args = (xBandPins, float(sys.argv[1]), xBand_raw_data_queue, xBand_fr_transform_queue, 'X-Band detector'))
+xBandThread = threading.Thread(target = xband_pir, args = (xBandPins, xBand_raw_data_queue, xBand_fr_transform_queue, xBand_detect_signal_queue, 'X-Band detector'))
 #pirThread = threading.Thread(target = xband_pir, args = (pirPins, int(sys.argv[1]), pirData_queue, 'PIR sensor'))
 
 xBandThread.start()
@@ -104,6 +134,7 @@ xBandThread.join()
 
 xBand_raw_data = xBand_raw_data_queue.get()
 xBand_fr_transform = xBand_fr_transform_queue.get()
+xBand_detect_signal = xBand_detect_signal_queue.get()
 #pirData = pirData_queue.get()
 
 file = open("plot_data" + "_"  + ".txt", "w")
@@ -112,6 +143,11 @@ for index in range(len(xBand_fr_transform[0])):
     file.write(str(xBand_fr_transform[0][index]) + s + str(xBand_fr_transform[1][index]) + "\n")
 file.close()
 
+file = open("detect_signal" + "_"  + ".txt", "w")
+s = ' '
+for index in range(len(xBand_detect_signal[0])):
+    file.write(str(xBand_detect_signal[0][index]) + s + str(xBand_detect_signal[1][index]) + s + str(xBand_detect_signal[2][index]) + s + str(xBand_detect_signal[3][index]) +"\n")
+file.close()
 
 
 
--- new_plot.py
+++ new_plot.py
@@ -13,20 +13,44 @@ import math
 xBand_time = []
 xBand_fr = []
 
+xBand_time1 = []
+xBand_s = []
+std_graph = []
+mean_graph = []
+
 raw_data = open("plot_data_" + ".txt", "r")
 for line in raw_data:
 	string = line.split()
 	xBand_time.append(string[0])
 	xBand_fr.append(string[1])
 
+raw_data1 = open("detect_signal_" + ".txt", "r")
+for line in raw_data1:
+	string = line.split()
+	xBand_time1.append(string[0])
+	xBand_s.append(string[1])
+	mean_graph.append(string[2])
+	std_graph.append(string[3])
+
 gs1 = GridSpec(1, 1)
 gs1.update(left=0.03, right=0.98, wspace=0)
 
 fr_tr = plt.subplot(gs1[0])
-fr_tr.plot(xBand_time, xBand_fr, 'r')
+fr_tr.plot(xBand_time, xBand_fr, 'b')
+fr_tr.plot(xBand_time1, xBand_s, 'r')
+fr_tr.plot(xBand_time1, mean_graph, 'k')
+fr_tr.plot(xBand_time1, std_graph, 'g')
+
 plt.ylabel('Motion status')
 plt.title('Frequency transformation')
 
+'''
+fr_ = plt.subplot(gs1[1])
+
+fr_.plot(xBand_time1, std_graph, 'g')
+plt.ylabel('Motion status')
+plt.title('Frequency transformation')
+'''
 plt.show()
 
 
